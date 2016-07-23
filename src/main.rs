901a0bd2126178078276ab0177a46dfef457d1fc
Mateu Adsuara <mateuadsuara@gmail.com>
2016-07-23 15:12:25 +0200

    Display cube

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
 #[macro_use]
 extern crate glium;
 
-use glium::{DisplayBuild, Surface, Program, VertexBuffer};
-use glium::index::{PrimitiveType, NoIndices};
+use glium::{DisplayBuild, Surface, Program, VertexBuffer, IndexBuffer};
+use glium::index::{PrimitiveType};
 use glium::backend::glutin_backend::{GlutinFacade};
 use glium::glutin::{WindowBuilder, Event, ElementState, VirtualKeyCode};
 
 fn main() {
     let display = create_display();
 
-    let shape = vec![
-        vec2(-0.5, -0.5),
-        vec2( 0.0,  0.5),
-        vec2( 0.5, -0.25)
-    ];
-
-    let mut offset: f32 = -0.5;
     loop {
-        offset = update_offset(offset);
-        draw(&display, &shape, offset);
+        draw(&display, &cube(), 0.0);
 
         for ev in display.poll_events() {
             match ev {
@@ -29,26 +21,63 @@ fn main() {
     }
 }
 
-fn update_offset(offset: f32) -> f32 {
-    let mut t: f32 = offset;
+struct Shape {
+    vertices: Vec<Vertex>,
+    indices: Vec<u32>
+}
 
-    t += 0.0002;
-    if t > 0.5 {
-        t = -0.5;
-    }
+fn plane() -> Shape {
+    return Shape {
+        vertices: vec![
+            vec3(-1.0,-1.0, 0.0),
+            vec3(-1.0, 1.0, 0.0),
+            vec3( 1.0,-1.0, 0.0),
+            vec3( 1.0, 1.0, 0.0),
+        ],
+        indices: vec![
+            0, 1, 2,
+            1, 2, 3
+        ]
+    };
+}
 
-    return t;
+fn cube() -> Shape {
+    return Shape {
+        vertices: vec![
+            vec3(0.0, 0.0, 0.0),
+            vec3(0.0, 0.0, 1.0),
+            vec3(0.0, 1.0, 0.0),
+            vec3(0.0, 1.0, 1.0),
+            vec3(1.0, 0.0, 0.0),
+            vec3(1.0, 0.0, 1.0),
+            vec3(1.0, 1.0, 0.0),
+            vec3(1.0, 1.0, 1.0),
+        ],
+        indices: vec![
+            1,7,5,
+            1,3,7, 
+            1,4,3, 
+            1,2,4, 
+            3,8,7, 
+            3,4,8, 
+            5,7,8, 
+            5,8,6, 
+            1,5,6, 
+            1,6,2, 
+            2,6,8, 
+            2,8,4, 
+        ]
+    };
 }
 
 #[derive(Copy, Clone)]
 struct Vertex {
-    position: [f32; 2],
+    position: [f32; 3],
 }
-
 implement_vertex!(Vertex, position);
 
-fn vec2(x: f32, y: f32) -> Vertex {
-    return Vertex { position: [x, y] };
+fn vec3(x: f32, y: f32, z: f32) -> Vertex {
+    return Vertex { position: [x, y, z] };
 }
 
 fn create_display() -> GlutinFacade {
@@ -58,12 +87,12 @@ fn create_display() -> GlutinFacade {
         .build_glium().unwrap();
 }
 
-fn draw(display: &GlutinFacade, shape: &Vec<Vertex>, offset: f32) {
+fn draw(display: &GlutinFacade, shape: &Shape, offset: f32) {
     let vertex_shader_src = r#"
         #version 140
 
         uniform float offset;
-        in vec2 position;
+        in vec3 position;
 
         void main() {
             gl_Position = vec4(position.x + offset, position.y, 0.0, 1.0);
@@ -84,8 +113,8 @@ fn draw(display: &GlutinFacade, shape: &Vec<Vertex>, offset: f32) {
     frame.clear_color(0.0, 0.0, 0.0, 0.0);
 
     frame.draw(
-        &VertexBuffer::new(display, shape).unwrap(),
-        &NoIndices(PrimitiveType::TrianglesList),
+        &VertexBuffer::new(display, &shape.vertices).unwrap(),
+        &IndexBuffer::new(display, PrimitiveType::TrianglesList, &shape.indices).unwrap(),
         &Program::from_source(display, vertex_shader_src, fragment_shader_src, None).unwrap(),
         &uniform! {offset: offset},
         &Default::default()
